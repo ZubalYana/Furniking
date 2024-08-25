2d9378545c7612c39764975d032b345337758e8b
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-08-25 15:30:26 +0300

    apply the server styles, open the page

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
-const dotenv = require('dotenv').config()
+require('dotenv').config()
 const PORT = process.env.PORT || 3000;
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname, 'public', 'index.html')
+})
 app.listen(PORT, (()=>{
     console.log(`Server works on PORT: ${PORT}`)
 }))
