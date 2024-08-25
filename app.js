75a7aee5346ffb99895865c64142c20d09d6420a
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-08-25 17:15:30 +0300

    connect to the database

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT || 3000;
 const path = require('path')
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb+srv://zubalana0:${process.env.password}@cluster0.a50jr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+.then(()=>{
+    console.log(`Connected to MongoDB`)
+})
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req,res)=>{
     res.sendFile(__dirname, 'public', 'index.html')
