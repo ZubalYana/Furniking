c5d3ff8daa688aa5f864ef59f84c932c4c640fbb
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-08-29 11:15:45 +0300

    create the goods schema

---
+++ models/Goods.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const goodsSchema = new mongoose.Schema({
+    img: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    prices: {
+        type: Array,
+        required: true
+    },
+})
+
+const Goods = mongoose.Model('Goods', goodsSchema)
+module.exports(Goods)
