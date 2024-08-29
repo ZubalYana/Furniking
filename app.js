af41d9d8fabebf633cfefa29ddbe48b578e7c51a
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-08-29 11:29:50 +0300

    connect multer and create it's storage

--- app.js
+++ app.js
@@ -5,11 +5,24 @@ const PORT = process.env.PORT || 3000;
 const path = require('path')
 const mongoose = require('mongoose')
 const Goods = require('./models/Goods')
+const multer = require('multer')
 mongoose.connect(`mongodb+srv://zubalana0:${process.env.password}@cluster0.a50jr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
 .then(()=>{
     console.log(`Connected to MongoDB`)
 })
 app.use(express.static(path.join(__dirname, 'public')))
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); 
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + '-' + file.originalname);
+    }
+});
+const upload = multer({ storage: storage });
+
 app.get('/', (req,res)=>{
     res.sendFile(__dirname, 'public', 'index.html')
 })
